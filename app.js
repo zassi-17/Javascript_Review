7a8f26bf7955a6130c558ec595c86fbd70f435b0
Ubuntu <sgk.k.s.17@gmail.com>
2025-11-29 05:17:24 +0000

    演算式の復習

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert('Hello world');
+let orange = '100';
+let apple = '200';
+
+if (orange < apple){
+  alert('みかんの値段がりんごより安い');
+}
+else if (orange == apple){
+  alert('みかんとりんごは同じ値段です');
+}
+else {
+  alert('みかんの値段はりんごより高い');
+}
