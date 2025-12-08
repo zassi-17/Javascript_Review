e2ade530edbb75441e67f4d3f1d354cf79d5d166
Ubuntu <sgk.k.s.17@gmail.com>
2025-12-08 05:29:05 +0000

    マウスオーバーイベントの復習

--- box.js
+++ box.js
@@ -1,9 +1,6 @@
 $(function (){
-  $('.box1').slideDown(1000, function (){
-      $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp(1000);
+  $('.box1').mouseover(function (){
+    $('.box1').css({'background-color': '#0000FF'
+    });
   });
 });
