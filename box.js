752fe944da2eee081e63324492baa675518b6bed
Ubuntu <sgk.k.s.17@gmail.com>
2025-12-08 05:57:47 +0000

    thisの復習

--- box.js
+++ box.js
@@ -1,9 +1,5 @@
 $(function (){
   $('.box1').on('click',function (){
-    $('.box1').addClass('box1-ext');
-  });
-
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
+    $(this).slideUp();
   });
 });
