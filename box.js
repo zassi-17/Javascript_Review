9213abcef41ad0db0b406bae146429a91419cf70
Ubuntu <sgk.k.s.17@gmail.com>
2025-12-08 05:40:56 +0000

    マウスアウトイベント・Class 属性の復習

--- box.js
+++ box.js
@@ -1,6 +1,9 @@
 $(function (){
   $('.box1').mouseover(function (){
-    $('.box1').css({'background-color': '#0000FF'
-    });
+    $('.box1').addClass('box1-ext');
+  });
+
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
