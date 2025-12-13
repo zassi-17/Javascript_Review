a3e899472d97605157e7117fcf65b685db3ff233
Ubuntu <sgk.k.s.17@gmail.com>
2025-12-13 08:20:11 +0000

    スクロールボタンの復習

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-const swiper = new Swiper('.swiper', {
-  // 最後までスライドしたら最初の画像の戻る
-  loop: true,
-
-  // ページネーションの表示
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // ナビゲーションボタンの表示
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
+$(function () {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    },1000);
+    event.preventDefault();
+  });
 });
